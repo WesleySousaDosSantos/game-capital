9cef2dcb0c7d8b6459e41487e4c5d92661d2228a
WesleySousaDosSantos <wesley43sousa@gmail.com>
2024-11-23 14:49:54 -0300

    capital

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [MainController::class, 'startGame'])->name('start_game');
+Route::post('/', [MainController::class, 'prepareGame'])->name('prepare_name');
+
+Route::get('/game', [MainController::class, 'game'])-> name('game');
+Route::get('/answer{answer}', [MainController::class, 'answer'])->name('answer');
+Route::get('/next_question', [MainController::class, 'nextQuestion'])->name('next_question');
+
+Route::get('/show_result', [MainController::class, 'showResults'])->name('show_results');
